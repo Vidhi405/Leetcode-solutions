fc3dcfad77e6ab403a33b37e506da77b08a0eeb6
Vidhi Srivastava <145354659+Vidhi405@users.noreply.github.com>
2025-12-08 19:24:35 +0530

    Create 1925.cpp

---
+++ 1925.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countTriples(int n) {
+
+        int count=0;
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                double k=sqrt(i*i + j*j);
+                if(k<=n && k==(int)k)
+                    count+=2;
+            }
+        }
+
+        return count;
+    }
+};
